08b22f8b7aea0b8f24c04f485929898b8e9111c2
brayanyaquian <bragonya@gmail.com>
2022-08-02 15:03:30 -0600

    search ignoring case

--- app/src/main/java/com/example/stubhub/repositories/EventsRepository.kt
+++ app/src/main/java/com/example/stubhub/repositories/EventsRepository.kt
@@ -9,6 +9,6 @@ import javax.inject.Inject
 class EventsRepository @Inject constructor(val fileManager: FileManager){
     fun getEvents(name: String, price: String) = Json
         .decodeFromString<List<Event>>(fileManager.readFromAsset("input.json"))
-        .filter { name.isBlank() || it.artiste.contains(name, ignoreCase = true) }
+        .filter { name.isBlank() || it.city.contains(name, ignoreCase = true) }
         .filter { price.isBlank() || it.price <= price.toLong() }
 }
